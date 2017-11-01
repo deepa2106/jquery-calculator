c89a6238c2d40a12a8d009721ac86c43e99560b8
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-11-01 20:56:31 +0000

    Numbers and operators stored

--- js/calc.js
+++ js/calc.js
@@ -2,22 +2,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var numberButtons = document.querySelectorAll('.buttonNum');
 
+  var number1 = 0;
+  var number2 = 0;
+
   for (var i = 0; i < numberButtons.length; i++) {
     var numberButton = numberButtons[i];
     numberButton.addEventListener('click', function(event) {
       var display = document.getElementById("screen");
-      display.innerHTML = this.value;
+
+      number1 = (number1 *  10) + parseInt(this.value);
+      display.innerHTML = number1;
     });
   }
 
   var operatorButtons = document.querySelectorAll('.operator');
+
+  var operation;
 
   for (var i = 0; i < operatorButtons.length; i++) {
     var operatorButton = operatorButtons[i];
     operatorButton.addEventListener('click', function(event) {
       var display = document.getElementById("screen");
       display.innerHTML = this.value;
+      operation = this.value;
+      number2 = number1;
+      number1 = 0;
     });
   }
 
+  var equalsButton = document.querySelectorAll('.equals');
+  equalsButton[0].addEventListener('click', function(event) {
+    var display = document.getElementById("screen");
+    display.innerHTML = this.value;
+  });
 });
